452f05b0255b905e7e9cc0d96f864c33e34ed719
Olivia <olivialuciano42@gmail.com>
2026-04-25 21:05:21 -0300

    cambio la url de la api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://www.buscoprofeapi.somee.com/api";
+export const API_BASE_URL = "https://buscoprofeapi.somee.com/api";
 
 export const ROLES = {
   ADMIN: "Admin",
